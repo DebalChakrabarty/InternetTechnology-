14adc6af8b6a62cfdcc2d811f9b20a449f63dc20
DebalChakrabarty <debalchakrabarty@gmail.com>
2017-10-19 12:16:00 +0530

    ADD size validation

--- assignment1/form.js
+++ assignment1/form.js
@@ -10,19 +10,40 @@ function validate(){
     var e=/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
     var flag=true;
-    
+    if(firstName=="")
+    {
+            document.getElementById("firstValidation").innerHTML = "";
+    }
+
+    if(lastName=="")
+    {
+        document.getElementById("secondValidation").innerHTML = "";
+    }
+
     console.log("inside validate");
     if(firstName!="" || lastName!="")
     {
-        if(regex.test(firstName)== false && firstName.length<=40)
+        if(regex.test(firstName)== false)
         {
                 document.getElementById("firstValidation").innerHTML = "No special characters or numbers";
                 document.getElementById("firstName").value="";
                 flag=false;
         }
-        if(regex.test(lastName)== false && lastName.length<=40)
+        if(regex.test(lastName)== false)
         {
                 document.getElementById("secondValidation").innerHTML = "No special characters or numbers";
+                document.getElementById("lastName").value="";
+                flag=false;
+        }
+        if(firstName.length>40)
+        {
+                document.getElementById("firstValidation").innerHTML = "must be less than 40 characters";
+                document.getElementById("firstName").value="";
+                flag=false;
+        }
+        if(lastName.length>40)
+        {
+                document.getElementById("secondValidation").innerHTML = "must be less than 40 characters";
                 document.getElementById("lastName").value="";
                 flag=false;
         }
@@ -31,10 +52,14 @@ function validate(){
     {
         if(phn.test(ph)==false)
         {
-                document.getElementById("phoneValidation").innerHTML = "must be 10 digits only, no special characters or alphabets";
+                document.getElementById("phoneValidation").innerHTML = "10 digits only";
                 document.getElementById("phno").value="";
                 flag=false;
         }
+    }
+    else
+    {
+        document.getElementById("phoneValidation").innerHTML = "";
     }
     if(email!="")
     {
@@ -45,6 +70,11 @@ function validate(){
                 flag=false;
             }
     }
+    else
+    {
+            document.getElementById("emailValidation").innerHTML = "";
+    }
+
     if(flag && firstName!="" && lastName!="" && ph!="" && email!="" && password!="")
     {
             document.write("Form successfully submitted");
